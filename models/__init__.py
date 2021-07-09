4de689e1f24e23bf9102842faa586f04c1c86353
Florent_P <63802082+Prud-homme@users.noreply.github.com>
2021-07-09 18:42:41 +0200

    Delete models directory

--- models/__init__.py
+++
@@ -1,4 +0,0 @@
-from .match import Match
-from .player import Player
-from .tournament import Tournament
-from .turn import Turn
--- models/match.py
+++
@@ -1,7 +0,0 @@
-class Match:
-	def __init__(self, player1, score1, player2, score2):
-		""""""
-		self.match = ([player1, score1], [player2, score2])
-
-	def __str__(self):
-		return f"Match: {self.match}"
