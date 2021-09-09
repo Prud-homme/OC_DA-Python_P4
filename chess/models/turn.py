5279a741e2a7ee02846e9f884227e6cc35bfd0b4
Florent_P <63802082+Prud-homme@users.noreply.github.com>
2021-09-09 19:58:47 +0200

    Update turn model
    Add method: __str__() ; __repr__() ; display(),  change method name: stop_turn()

--- chess/models/turn.py
+++ chess/models/turn.py
@@ -1,50 +1,55 @@
+from typing import Optional  # , NewType, Tuple, List
 from datetime import datetime
 
-from chess.models import Match
+# TournamentMatch = NewType('TournamentMatch', Tuple[List[int,int],List[int,int]])
 
 
 class Turn:
-    def __init__(self, name=None):
-        """"""
-        self.name = name
+    def __init__(self, **kwargs) -> None:
+        ''''''
+        self.name = kwargs.get('name', None)
         self.start_date = self.get_current_time()
-        self.matchs = []
         self.end_date = None
+        self.matchs = []
 
-    def add_match(self, match):
-        """"""
+    def __str__(self) -> None:
+        return f'''The tour is called {self.name} and started on {self.start_date}.'''
+
+    def __repr__(self) -> None:
+        return f'''Turn(name='{self.name}')'''
+
+    def add_match(self, match: tuple) -> None:
+        ''''''
         self.matchs.append(match)
 
-    def close_turn(self):
-        """"""
+    def stop_turn(self) -> None:
+        ''''''
         self.end_date = self.get_current_time()
 
-    def serializing(self):
-        """"""
-        matchs = []
-        for match in self.matchs:
-            matchs.append(match.serializing())
+    def serializing(self, serial_matchs: dict) -> dict:
+        ''''''
         return {
             'name': self.name,
             'start_date': self.start_date,
             'end_date': self.end_date,
-            'matchs': matchs
+            'matchs': serial_matchs,
         }
 
-    def unserializing(self, turn_data):
-        """"""
-        matchs = []
-        for match in turn_data['matchs']:
-            ([player1, score1], [player2, score2]) = match
-            matchs.append(Match(player1, score1, player2, score2))
+    def unserializing(self, serial_turn: dict, unserial_matchs: list) -> None:
+        ''''''
+        self.name = serial_turn['name']
+        self.start_date = serial_turn['start_date']
+        self.matchs = unserial_matchs
+        self.end_date = serial_turn['end_date']
 
-        self.name = turn_data['name']
-        self.start_date = turn_data['start_date']
-        self.matchs = matchs
-        self.end_date = turn_data['end_date']
+    def display(self)
+        if self.name != None:
+            print(f'\n{self.__str__()}\n')
+        else:
+            print('The turn is not correctly defined, try again after completing his information')
 
     @staticmethod
-    def get_current_time():
-        """"""
+    def get_current_time() -> str:
+        ''''''
         now = datetime.now()
-        return now.strftime("Date: %d/%m/%Y, Time: %H:%M")  # :%S")
+        return now.strftime('Date: %d/%m/%Y, Time: %H:%M')
