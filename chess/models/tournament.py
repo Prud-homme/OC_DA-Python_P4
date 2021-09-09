5b54216e0acae6cb75133474a66d215f3936411d
Florent_P <63802082+Prud-homme@users.noreply.github.com>
2021-09-09 19:56:42 +0200

    Update tournament model
    Return only None or boolean,  update method __str__() and __repr__(),  change method name: attributes_are_not_none(),  add method: display() ; associate_player_ids_with_their_name()

--- chess/models/tournament.py
+++ chess/models/tournament.py
@@ -1,76 +1,76 @@
 import re
 from textwrap import dedent
+from typing import Optional  # , NewType, Tuple, List, Dict, Union
 
-from chess.models import Turn
-from chess.settings import PLAYERS_TABLE, TIME_CONTROL, TOURNAMENTS_TABLE
+from chess.settings import TIME_CONTROL
+
+# TournamentMatch = NewType('TournamentMatch', Tuple[List[int,int],List[int,int]])
+# TournamentTurn = NewType('TournamentTurn', Union[str,str,str,List[TournamentMatch]])
 
 
 class Tournament:
-
-    def __init__(
-            self,
-            name=None,
-            location=None,
-            date=None,
-            description=None,
-            time_control=None,
-            turns_number=4,
-            players_number=8):
-        self.name = name
-        self.location = location
-        self.date = date
-        self.description = description
-        self.time_control = time_control
-        self.turns_number = turns_number
-        self.players_number = players_number
+    def __init__(self, **kwargs) -> None:    
+        self.name =  kwargs.get('name', None)
+        self.location = kwargs.get('location', None)
+        self.date = kwargs.get('date', None)
+        self.description = kwargs.get('description', None)
+        self.time_control = kwargs.get('time_control', None)
+        self.turns_number = kwargs.get('turns_number', 4)
+        self.players_number = kwargs.get('players_number', 8)
         self.players = []
         self.turns = []
 
-    def __str__(self):
-        info = f'''Tournament informations:
-		Name: {self.name}
-		Location: {self.location}
-		Date: {self.date}
-		Description: {self.description}
-		Time control: {self.time_control}
-		Turns number: {self.turns_number}
-		Players number: {self.players_number}
-		Players defined: {self.players}'''
-        return dedent(info)
+    def __str__(self) -> None:
+        return f'''The tournament is called {self.name}
+and takes place in {self.location} on {self.date}.
+Description of the tournament: {self.description}
+It will take place in {self.turns_number} turns 
+with {self.players_number} players
+and {self.time_control} as time control.'''.replace('\n', ' ')
 
-    def add_turn(self, turn):
-        """"""
+    def __repr__(self) -> None:
+        return f'''Tournament(name='{self.name}', location='{self.location}',
+date='{self.date}', description='{self.description}',
+time_control='{self.time_control}', turns_number={self.turns_number},
+players_number={self.players_number})'''.replace('\n', ' ')
+
+    def add_turn(self, turn) -> None:
+        ''''''
         self.turns.append(turn)
 
-    def add_player(self, player_id):
-        """"""
-        self.players.append(player_id)
+    def add_players(self, players_table, views_player, player)->None:
+        choice = -1
+        while choice != 0 or len(self.players) != self.players_number:
+            choice = views_player.display_menu_add_player()
 
-    def partial_serializing(self):
-        """"""
-        return {
-            'name': self.name,
-            'location': self.location,
-            'date': self.date,
-            'description': self.description,
-            'time_control': self.time_control,
-            'turns_number': self.turns_number,
-            'players_number': self.players_number
-        }
+            # Add by id
+            if choice == 1:
+                self.players.append(views_player.enter_player_id())
 
-    def partial_well_defined(self):
-        return None not in (
-            self.name,
-            self.location,
-            self.date,
-            self.description,
-            self.time_control)
+            # Add by name
+            elif choice == 2:
+                firstname = views_player.enter_player_firstname(search=True)
+                lastname = views_player.enter_player_lastname(search=True)
+                result = players_table.search_by_first_and_last_name(
+                    firstname, lastname)
+                player_selected = views_player.display_search_result(result)
+                if player_selected != None:
+                    self.players.append(
+                        players_table.get_id(
+                            result[player_selected]))
 
-    def serializing(self):
-        """"""
-        turns = []
-        for turn in self.turns:
-            turns.append(turn.serializing)
+            # Create and add
+            elif choice == 3:
+                player.firstname, player.lastname, player.birthdate, player.gender, player.ranking = views_player.define_new_player()
+                valid_insert = players_table.create_item(player.serializing())
+                if valid_insert:
+                    self.players.append(player.get_id_in_database())
+
+            if len(self.players) == self.players_number:
+                print('The list of players for this tournament is complete.')
+
+    def serializing(self, serial_turns: dict) -> None:
+        ''''''
         return {
             'name': self.name,
             'location': self.location,
@@ -80,28 +80,22 @@ class Tournament:
             'turns_number': self.turns_number,
             'players_number': self.players_number,
             'players': self.players,
-            'turns': turns
+            'turns': serial_turns,
         }
 
-    def unserializing(self, tournament_data):
-        """"""
-        turns = []
-        for serial_turn in tournament_data['turns']:
-            turn = Turn()
-            turn.unserializing(serial_turn)
-            turns.append(turn)
+    def unserializing(self, serial_tournament: dict, unserial_turns: list) -> None:
+        ''''''
+        self.name = serial_tournament['name']
+        self.location = serial_tournament['location']
+        self.date = serial_tournament['date']
+        self.description = serial_tournament['description']
+        self.time_control = serial_tournament['time_control']
+        self.turns_number = serial_tournament['turns_number']
+        self.players_number = serial_tournament['players_number']
+        self.players = serial_tournament['players']
+        self.turns = unserial_turns
 
-        self.name = tournament_data['name']
-        self.location = tournament_data['location']
-        self.date = tournament_data['date']
-        self.description = tournament_data['description']
-        self.time_control = tournament_data['time_control']
-        self.turns_number = tournament_data['turns_number']
-        self.players_number = tournament_data['players_number']
-        self.players = tournament_data['players']
-        self.turns = turns
-
-    def load_scores(self):
+    def load_scores(self) -> list:
         scores = [0] * len(self.players)
         for turn in self.turns:
             for match in turn.matchs:
@@ -112,42 +106,112 @@ class Tournament:
                 scores[index2] += float(score2)
         return scores
 
-    def load_ranking(self):
+    def load_ranking(self, players_table) -> list:
         ranking = []
         for player_id in self.players:
-            player_data = PLAYERS_TABLE.get_item_with_id(player_id)
+            player_data = players_table.get_item_with_id(player_id)
             ranking.append(int(player_data['ranking']))
         return ranking
 
-    def exist_in_database(self):
-        if self.partial_well_defined():
-            return TOURNAMENTS_TABLE.exist_serial_data(
-                self.partial_serializing())
+    def serialised_information_for_research(self) -> dict:
+        ''''''
+        return {
+            'name': self.name,
+            'location': self.location,
+            'date': self.date,
+            'description': self.description,
+            'time_control': self.time_control,
+            'turns_number': self.turns_number,
+            'players_number': self.players_number,
+        }
+
+    def attributes_are_not_none(self) -> bool:
+        return None not in (
+            self.name,
+            self.location,
+            self.date,
+            self.description,
+            self.time_control,
+        )
+
+    def exist_in_database(self, tournaments_table) -> bool:
+        if self.attributes_are_not_none():
+            return tournaments_table.exist_serial_data(
+                self.serialised_information_for_research()
+            )
         return False
 
-    def get_id_in_database(self):
-        if self.partial_well_defined():
-            return TOURNAMENTS_TABLE.get_id(self.partial_serializing())
-        return -1
+    def get_id_in_database(self, tournaments_table) -> Optional[int]:
+        if self.attributes_are_not_none():
+            return tournaments_table.get_id(self.serialised_information_for_research())
+        return None
 
-    def insert_in_database(self):
-        if self.partial_well_defined():
-            TOURNAMENTS_TABLE.create_item(self.serializing())
-            return 'Success'
+    def insert_in_database(self, tournaments_table) -> None:
+        if not self.exist_in_database():
+            tournaments_table.create_item(self.serializing())
+            print('Successful insert.')
         else:
-            return 'Fail: tournament is not well defined'
+            print(
+                'Insertion impossible, the tournament already exists in the database.'
+            )
 
-    def load_from_database_with_serial_data(self, serial_data):
-        if TOURNAMENTS_TABLE.exist_serial_data(serial_data):
-            self.unserializing(serial_data)
-            return 'Success'
+    def load_from_database_with_id(self, tournaments_table, tournament_id: int) -> None:
+        if tournaments_table.exist_id(tournament_id):
+            self.unserializing(tournaments_table.get_item_with_id(tournament_id))
+            print('Successful load.')
         else:
-            return 'Fail: no tournament exists with this data in the database'
+            print('Load impossible, player does not exist in the database.')
 
-    def update_in_database(self):
+    def load_from_database_with_serial_data(
+        self, tournaments_table, serial_tournament: dict, unserial_turns
+    ) -> None:
+        if tournaments_table.exist_serial_data(serial_tournament):
+            self.unserializing(serial_tournament, unserial_turns)
+            print('Successful load.')
+        else:
+            print('Load impossible, tournament does not exist in the database.')
+
+    def update_in_database(self, tournaments_table) -> None:
         tournament_id = self.get_id_in_database()
-        if tournament_id != -1:
-            TOURNAMENTS_TABLE.update_item(self.serializing(), tournament_id)
-            return 'Success'
+        if tournament_id != None:
+            tournaments_table.update_item(self.serializing(), tournament_id)
+            print('Successful update.')
         else:
-            return 'Fail: no tournament exists with this data in the database'
+            print('Update impossible, tournament does not exist in the database.')
+
+    def display(self)->None:
+        if self.attributes_are_not_none():
+            print(f'\n{self.__str__()}\n')
+        else:
+            print('The tournament is not correctly defined, try again after completing his information')
+
+    def associate_player_ids_with_their_name(self, players_table):
+        players_id_with_name = {}
+        for player_id in self.players:
+            player_data = players_table.get_item_with_id(player_id)
+            if player_data != None:
+                players_id_with_name[player_id] = ' '.join((player_data['firstname'],player_data['lastname']))
+        if len(players_id_with_name) == len(self.players):
+            return players_id_with_name
+        else:
+            return None
+
+
+if __name__ == '__main__':
+    tournament1 = Tournament(
+        'Tournament 1',
+        'Paris',
+        '2019/09/22',
+        'Qualification for Tournament 2.',
+        'bullet',
+        8,
+        16,
+    )
+    print(f'__str__:\n{tournament1.__str__()}\n')
+    print(f'__repr__:\n{tournament1.__repr__()}\n')
+
+    tournament2 = Tournament(
+        'Tournament 2', 'Paris', '2019/10/12', 'Regional championship', 'bullet'
+    )
+    print(f'__str__:\n{tournament2.__str__()}\n')
+    print(f'__repr__:\n{tournament2.__repr__()}\n')
