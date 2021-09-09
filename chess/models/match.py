5c8bf2b7a5094a818cc3898a94a6f0e5f2fc0600
Florent_P <63802082+Prud-homme@users.noreply.github.com>
2021-09-09 19:51:08 +0200

    Update match model
    method __init__() use **kwargs, add method : __str__() ; __repr__() ; attributes_are_not_none() ; display()

--- chess/models/match.py
+++ chess/models/match.py
@@ -1,10 +1,33 @@
 class Match:
-    def __init__(self, player1, score1, player2, score2):
-        """"""
+    def __init__(self, **kwargs) -> None:
+        ''''''
+        player1 = kwargs.get('player1', None)
+        score1 = kwargs.get('score1', None)
+        player2 = kwargs.get('player2', None)
+        score2 = kwargs.get('score2', None)
         self.match = ([player1, score1], [player2, score2])
 
-    def serializing(self):
-        """"""
-        return {
-            'match': self.match
-        }
+    def __str__(self) -> None:
+        [player1, score1], [player2, score2] = self.match
+        return f'''The match was between {player1} and {player2}.
+{player1} got {score1} point and {player2} got {score2} points.'''.replace('\n', ' ')
+
+    def __repr__(self) -> None:
+        [player1, score1], [player2, score2] = self.match
+        return f'''Match(player1='{player1}', score1='{score1}', 
+player2='{player2}', score1='{score2}')'''.replace('\n', ' ')
+
+    def serializing(self) -> dict:
+        ''''''
+        return {'match': self.match}
+
+    def attributes_are_not_none(self):
+        [player1, score1], [player2, score2] = self.match
+        return None in (player1, score1, player2, score2)
+
+    def display(self)
+        if self.attributes_are_not_none():
+            print(f'\n{self.__str__()}\n')
+        else:
+            print('The match is not correctly defined, try again after completing his information')
+
