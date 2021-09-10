40283333794f3ed2981ed7d53690117ff1ae0c69
Florent_P <63802082+Prud-homme@users.noreply.github.com>
2021-09-10 11:34:56 +0200

    Update turn model
    Adding static methods: serializing and unserializing turn list

--- chess/models/turn.py
+++ chess/models/turn.py
@@ -8,15 +8,16 @@ class Turn:
     def __init__(self, **kwargs) -> None:
         ''''''
         self.name = kwargs.get('name', None)
-        self.start_date = self.get_current_time()
-        self.end_date = None
-        self.matchs = []
+        self.start_date = kwargs.get('start_date', self.get_current_time())
+        self.end_date = kwargs.get('end_date', None)
+        self.matchs = kwargs.get('matchs', [])
 
     def __str__(self) -> None:
-        return f'''The tour is called {self.name} and started on {self.start_date}.'''
+        return f'''The turn is called {self.name} and started on {self.start_date}.
+The turn ended on {self.end_date} and the matchs are: {str(self.matchs)[1:-1]}.'''
 
     def __repr__(self) -> None:
-        return f'''Turn(name='{self.name}')'''
+        return f'''Turn(name='{self.name}', start_date='{self.start_date}', end_date='{self.end_date}', matchs={self.matchs})'''
 
     def add_match(self, match: tuple) -> None:
         ''''''
@@ -53,3 +54,17 @@ class Turn:
         ''''''
         now = datetime.now()
         return now.strftime('Date: %d/%m/%Y, Time: %H:%M')
+
+    @staticmethod
+    def serializing_turns(match_object,turns):
+        return {'turns': [turn.serializing(match_object.serializing_matchs(self.matchs)) for turn in turns]}
+
+    @staticmethod
+    def unserializing_turns(match_object,serial_turns):
+        turns = []        
+        for serial_turn in serial_turns:
+            name, start_date, end_date = serial_turn['name'], serial_turn['start_date'], serial_turn['end_date']
+            matchs = match_object.unserializing_matchs(serial_turn['matchs'])
+            turns.append(Turn(name=name,start_date=start_date,end_date=end_date,matchs=matchs))
+
+        return turns
