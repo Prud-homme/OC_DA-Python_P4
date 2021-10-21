3475b672f7c24168bdacd155306c716f6e860d11
Florent_P <63802082+Prud-homme@users.noreply.github.com>
2021-10-21 19:24:30 +0200

    Update settings, Add utils

--- chess/settings.py
+++ chess/settings.py
@@ -4,4 +4,3 @@ DATABASE = "../test.json"
 TOURNAMENTS_TABLE = Table("Tournaments", DATABASE)
 PLAYERS_TABLE = Table("Players", DATABASE)
 TIME_CONTROL = ["bullet", "blitz", "coup rapide"]
-SCORE_VALUES = ["0", "0.5", "1/2", "0,5", "1"]
---
+++ chess/utils.py
@@ -0,0 +1,5 @@
+import os
+
+cls = lambda: os.system("cls")
+def pause():
+    programPause = input("\x1b[35mPress the <ENTER> key to continue...\x1b[0m")
