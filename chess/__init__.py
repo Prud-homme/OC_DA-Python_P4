dd3cecad7b7319c77b0512e5e8880659fe1e4fa6
Prud-homme Florent <prudhomme.flo@gmail.com>
2021-11-12 14:48:22 +0100

    Update app __init__ file

--- chess/__init__.py
+++ chess/__init__.py
@@ -1,5 +1,3 @@
-# from .models import Tournament, Player, Turn, Match
-# from .database import Table
-# from .controllers import tournaments as t_c#, players as p_c
-# from .views import main_menu as mm_v, tournaments as t_v, players as p_v, reports as r_v
-# from .settings import DATABASE, TIME_CONTROL
+from .controllers import TournamentController, launch_report, choice_is_valid, edit_player_ranking
+from .views import display_menu, display_message
+from .utils import clear_display
