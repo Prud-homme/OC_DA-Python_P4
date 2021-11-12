f9462f2709bb3690873365b3004db351203189c4
Prud-homme Florent <prudhomme.flo@gmail.com>
2021-11-12 14:47:25 +0100

    Update utils file

--- chess/utils.py
+++ chess/utils.py
@@ -1,5 +1,16 @@
 import os
+import time
+from logger import loglevel
 
-cls = lambda: os.system("cls")
+clear_display = lambda: os.system("cls")
+
+
 def pause():
     programPause = input("\x1b[35mPress the <ENTER> key to continue...\x1b[0m")
+
+
+def autopause():
+    if loglevel in ["DEBUG", "INFO"]:
+        time.sleep(1.5)
+    else:
+        time.sleep(0)
